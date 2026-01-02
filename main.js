1aff961f5abc108bd75c64aa099f13a91fda7fe9
adrhmdlz <adrianalzidan35@gmail.com>
2026-01-02 13:48:12 +0700

    feat(main, utility): Added main.js and utility for render icon.

---
+++ main.js
@@ -0,0 +1,12 @@
+// Pages
+import './components/pages/home.js';
+
+// Layout
+import './components/layout/about-section/about.js';
+import './components/layout/experience-section/experience.js';
+
+// UI
+import './components/ui/navbar/navbar.js';
+import './components/ui/wrapper/wrapper.js';
+import './components/ui/capsule/capsule.js';
+import './components/ui/button/button.js';
